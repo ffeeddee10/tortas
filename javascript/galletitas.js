41e8c8e36d1e12ce81efc215bc88a88209f1016c
federico gimenez <ffeeddee_10@hotmail.com>
2022-10-22 21:54:52 -0300

    mejorando

--- javascript/galletitas.js
+++ javascript/galletitas.js
@@ -13,3 +13,17 @@ fetch('../galletitas.json')
             promogalletitas.innerHTML += informacion
         });
     })
+
+
+fetch('../box.json')
+    .then(response => response.json())
+    .then((data) => {
+        data.forEach((cakes) => {
+            const informacion = `
+                             <a href="${cakes.image}" target="_blank" class="promo-margin"><img src="${cakes.image}" alt="..." id="${cakes.id}"
+                                class="promo_item promo_item_1 imagenes"></a>
+                             `
+            const promobox = document.getElementById('promobox');
+            promobox.innerHTML += informacion
+        });
+    })
